8ce62a90b10d2c98f99b99112a2d7e167428c8b9
mywnajsldkf <mywnajsldkf@gmail.com>
2023-05-23 11:07:27 +0900

    ✨ feat : server_static #13
    - 정적 컨텐츠를 클라이언트에게 제공한다.
    - 지역 파일의 내용을 포함하고 있는 본체를 갖는 HTTP 응답을 보낸다.

--- tiny/tiny.c
+++ tiny/tiny.c
@@ -156,3 +156,27 @@ int parse_uri(char *uri, char *filename, char *cgiargs)
       return 0;
   }  
 }
+
+void serve_static(int fd, char *filename, int filesize)
+{
+  int srcfd;
+  char *srcp, filetype[MAXLINE], buf[MAXBUF];
+
+  /* Send response headers to client */
+  get_filetype(filename, filetype);
+  sprintf(buf, "HTTP/1.0 200 OK\r\n");
+  sprintf(buf, "%sServer: Tiny Web Server\r\n", buf);
+  sprintf(buf, "%sConnection: close\r\n", buf);
+  sprintf(buf, "%sContent-length: %d\r\n", buf, filesize);
+  sprintf(buf, "%sContent-type: %s\r\n\r\n", buf, filetype);
+  Rio_writen(fd, buf, strlen(buf));
+  printf("Response headers: \n");
+  printf("%s", buf);
+
+  /* Send response body to client */
+  srcfd = Open(filename, O_RDONLY, 0);
+  srcp = Mmap(0, filesize, PROT_READ, MAP_PRIVATE, srcfd, 0);
+  Close(srcfd);
+  Rio_writen(fd, srcp, filesize);
+  Munmap(srcp, filesize);
+}
