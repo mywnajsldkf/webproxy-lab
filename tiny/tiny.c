ee3f11aa75662b11cb0d65c5d0a3c14c5919bea3
mywnajsldkf <mywnajsldkf@gmail.com>
2023-05-23 11:00:28 +0900

    ✨ feat : clienterror #13
    - 에러메시지를 클라이언트에게 전달한다.

--- tiny/tiny.c
+++ tiny/tiny.c
@@ -92,3 +92,25 @@ void doit(int fd)
     serve_dynamic(fd, filename, cgiargs);
   }
 }
+
+// 에러메시지를 클라이언트에게 전달한다.
+void clienterror(int fd, char *cause, char *errnum, char *shortmsg, char *longmsg)
+{
+  char buf[MAXLINE], body[MAXBUF];
+
+  /* Build the HTTP response body */
+  sprintf(body, "<html><title>Tiny Error</title>");
+  sprintf(body, "%s<body bgcolor=""ffffff"">\r\n", body);
+  sprintf(body, "%s%s: %s\r\n", body, errnum, shortmsg);
+  sprintf(body, "%s<p>%s: %s\r\n", body, longmsg, cause);
+  sprintf(body, "%s<hr><em>The Tiny Web Server</em>\r\n", body);
+
+  /* Print the HTTP response */
+  sprintf(buf, "HTTP/1.0 %s %s\r\n", errnum, shortmsg);
+  Rio_writen(fd, buf, strlen(buf));
+  sprintf(buf, "Content-type: text/html\r\n");
+  Rio_writen(fd, buf, strlen(buf));
+  sprintf(buf, "Content-length: %d\r\n\r\n", (int)strlen(body));
+  Rio_writen(fd, buf, strlen(buf));
+  Rio_writen(fd, body, strlen(body));
+}
