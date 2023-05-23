cd3d1612b0b87368d1c28e918463be3739db7ec7
mywnajsldkf <mywnajsldkf@gmail.com>
2023-05-23 10:59:21 +0900

    ✨ feat : doit 함수 #13
    - 1개의 HTTP 트랜잭션을 처리한다.
    - 요청이 정적 또는 동적 컨텐츠를 위한 것인지 확인하고 클라이언트에게 제공한다.
       - 정적 컨텐츠: 보통 파일인가, 읽기 권한이 있는가?
       - 동적 컨텐츠: 파일이 실행 가능한가?

--- tiny/tiny.c
+++ tiny/tiny.c
@@ -41,3 +41,54 @@ int main(int argc, char **argv) {
     Close(connfd);  // line:netp:tiny:close
   }
 }
+
+// 1개의 HTTP 트랜잭션을 처리한다.
+void doit(int fd)
+{
+  int is_static;
+  struct stat sbuf;
+  char buf[MAXLINE], method[MAXLINE], uri[MAXLINE], version[MAXLINE];
+  char filename[MAXLINE], cgiargs[MAXLINE];
+  rio_t rio;
+
+  /* Read request line and headers */
+  Rio_readinitb(&rio, fd);
+  Rio_readlineb(&rio, buf, MAXLINE);
+  printf("Request headers:\n");
+  printf("%s", buf);
+  sscanf(buf, "%s %s %s", method, uri, version);
+  // 다른 메서드를 요청하면, 에러 메시지를 보내고, main 루틴으로 돌아온다.
+  if (strcasecmp(method, "GET")) {
+    clienterror(fd, method, "501", "Not implemented", "Tiny does not implement this method");
+    return;
+  }
+  read_requesthdrs(&rio);
+
+  /* Parse URI from GET request */
+  is_static = parse_uri(uri, filename, cgiargs);  // static or dynamic
+  // file이 디스크 상에 없으면, 에러 메시지를 즉시 클라이언트에게 보내고 반환한다.
+  if (stat(filename, &sbuf) < 0) {
+    clienterror(fd, filename, "404", "Not found", "Tiny couldn't find this file");
+    return;
+  }
+
+  if (is_static)  /* Serve static content*/
+  {
+    // 보통 파일이고, 읽기 권한인지 검증한다.
+    if (!(S_ISREG(sbuf.st_mode)) || !(S_IRUSR & sbuf.st_mode)){
+      clienterror(fd, filename, "403", "Forbidden", "Tiny couldn't read the file");
+      return;
+    }
+    // 맞다면 클라이언트에게 제공한다.
+    serve_static(fd, filename, sbuf.st_size);
+  }
+  else {
+    // 동적 컨텐츠에 대한 것이라면 파일이 실행 가능한지 검증하고
+    if (!(S_ISREG(sbuf.st_mode)) || !(S_IXUSR & sbuf.st_mode)){
+      clienterror(fd, filename, "403", "Forbidden", "Tiny couldn't run the CGI program");
+      return;
+    }
+    // 그렇다면 동적 컨텐츠를 제공한다.
+    serve_dynamic(fd, filename, cgiargs);
+  }
+}
