10c38e70880458f7d0c5ac0018c643dc37b91172
mywnajsldkf <mywnajsldkf@gmail.com>
2023-05-23 11:09:47 +0900

    ✨ feat : serve_dynamic #13
    - 동적 컨텐츠를 제공한다.

--- tiny/tiny.c
+++ tiny/tiny.c
@@ -203,3 +203,22 @@ void get_filetype(char *filename, char *filetype)
     strcpy(filetype, "text/plain");
   }
 }
+
+void serve_dynamic(int fd, char *filename, char *cgiargs)
+{
+  char buf[MAXLINE], *emptylist[] = {NULL};
+
+  /* Return first part of HTTP response */
+  sprintf(buf, "HTTP/1.0 200 OK\r\n");
+  Rio_writen(fd, buf, strlen(buf));
+  sprintf(buf, "Server: Tiny Web Server\r\n");
+  Rio_writen(fd, buf, strlen(buf));
+
+  if (Fork() == 0) { /* Child */
+    /* Real server would set all CGI vars here */
+    setenv("QUERY_STRING", cgiargs, 1);
+    Dup2(fd, STDOUT_FILENO);              /* Redirect stdout to client */
+    Execve(filename, emptylist, environ); /* Run CGI program*/
+  }
+  Wait(NULL);
+}
