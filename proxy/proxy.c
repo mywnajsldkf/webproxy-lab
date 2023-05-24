d844414806692dbfad95a982d440e1f55f2a804d
mywnajsldkf <mywnajsldkf@gmail.com>
2023-05-24 16:37:51 +0900

    📝 chores : 파일 경로 이동 #28

--- proxy/proxy.c
+++ proxy/proxy.c
@@ -1,3 +1,16 @@
+#include <stdio.h>
+#include "csapp.h"
+
+/* Recommended max cache and object sizes */
+#define MAX_CACHE_SIZE 1049000
+#define MAX_OBJECT_SIZE 102400
+
+/* You won't lose style points for including this long line in your code */
+static const char *user_agent_hdr =
+    "User-Agent: Mozilla/5.0 (X11; Linux x86_64; rv:10.0.3) Gecko/20120305 "
+    "Firefox/10.0.3\r\n";
+
+int main(int argc, char **argv) {
   int listenfd, connfd;
   char hostname[MAXLINE], port[MAXLINE];
   socklen_t clientlen;
@@ -17,3 +30,6 @@
     Getnameinfo((SA *)&clientaddr, clientlen, hostname, MAXLINE, port, MAXLINE, 0);
     printf("Accepted connection from (%s, %s)\n", hostname, port);
   }
+
+  return 0;
+}
