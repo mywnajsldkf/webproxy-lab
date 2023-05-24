cad59deab3a411e3022954f9cc8088af7f3ff7a5
mywnajsldkf <mywnajsldkf@gmail.com>
2023-05-24 16:37:05 +0900

    ✨ feat : port로 들어오는 connection listen #28

---
+++ proxy/proxy.c
@@ -0,0 +1,19 @@
+  int listenfd, connfd;
+  char hostname[MAXLINE], port[MAXLINE];
+  socklen_t clientlen;
+  struct sockaddr_storage clientaddr;
+
+  if (argc != 2) {
+    fprintf(stderr, "usage: %s <port>\n", argv[0]);
+    exit(1);
+  }
+  
+  listenfd = Open_listenfd(argv[1]);
+  
+  while (1)
+  {
+    clientlen = sizeof(clientaddr);
+    connfd = Accept(listenfd, (SA *)&clientaddr, &clientlen);
+    Getnameinfo((SA *)&clientaddr, clientlen, hostname, MAXLINE, port, MAXLINE, 0);
+    printf("Accepted connection from (%s, %s)\n", hostname, port);
+  }
